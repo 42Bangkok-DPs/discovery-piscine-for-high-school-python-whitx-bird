7eb57d01a58515fbcd1db20e438a272e7e46151b
whitx-bird <147967702+whitx-bird@users.noreply.github.com>
2024-10-08 11:44:36 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+Num = float(input("Emter your number. :"))
+if Num == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "Python is awesome"
+user_password = input("Enter your password: ")
+if user_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
