5e3630ed9d9f23b9abe158ca0c7dd9c798119a6e
whitx-bird <147967702+whitx-bird@users.noreply.github.com>
2024-10-08 11:43:21 +0700

    Delete cell02 directory

--- cell02/ex00/iszero.py
+++
@@ -1,5 +0,0 @@
-Num = float(input("Emter your number. :"))
-if Num == 0:
-    print("This number is equal to zero.")
-else:
-    print("This number is different from zero.")
--- cell02/ex01/isneg.py
+++
@@ -1,7 +0,0 @@
-Num = float(input("Enter the number:"))
-if Num < 0:
-    print("This number is negative.")
-elif Num > 0:
-    print("This number is positive.")
-else:
-    print("This number is both positive and negative.")
