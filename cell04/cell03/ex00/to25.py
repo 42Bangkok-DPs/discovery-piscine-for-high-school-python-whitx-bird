249471b90258f5d36ed2eb9e63760683af270e30
whitx-bird <147967702+whitx-bird@users.noreply.github.com>
2024-10-09 15:14:58 +0700

    Delete cell04/cell03 directory

--- cell04/cell03/ex00/to25.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-num = int(input("Enter a number less than 25 \n"))
-if num >= 25 :
-    print('Error')
-while num <= 25:
-    print("Inside the loop, my veriable is",num)
-    num = num + 1
-    
- 
